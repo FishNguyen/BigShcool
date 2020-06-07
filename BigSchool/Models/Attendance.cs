3ec0ac8f711551c4735521fc9d9333fee1c65b5b
Fiz <nguyen.khanhdu018@gmail.com>
2020-06-07 21:20:05 +0700

    trang 37

---
+++ BigSchool/Models/Attendance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BigSchool.Models;
+
+namespace BigSchool.Models
+{
+    public class Attendance
+    {
+        public Course Course { get; set; }
+        [Key]
+        [Column(Oder =1)]
+        public applicationUser Attendee { get; set; }
+        [Key]
+        [Column(Oder = 2)]
+        public string AttendeeID { get; set; }
+    }
+}
